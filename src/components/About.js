56b567777a06e3e040975bcae54d95540cbf8c3f
Tirsa St. Fort <lifelovelori@gmail.com>
2023-06-27 14:41:21 -0700

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Any content of your choosing</p>
+    <img src={image} alt="I made this"></img>About</div>;
 }
 
 export default About;
